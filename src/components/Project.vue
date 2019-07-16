c28b2f0595fc13404c1be0536c68f35b9f0905bc
huipaodejitui <920210993@qq.com>
2019-07-16 23:32:52 +0800

    feat:添加了微信跳一跳和flybird项目

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,175 +1,183 @@
 <template>
   <common :op="op">
-    <Work class="work" :op="workOp"/>
+    <Work class="work" :op="workOp" />
   </common>
 </template>
 
 <script>
-import Common from '../common/Common';
-import Work from '../common/Work';
+import Common from "../common/Common";
+import Work from "../common/Work";
 export default {
-  name: 'Project',
+  name: "Project",
   components: {
     Common,
     Work
   },
-  data () {
+  data() {
     return {
       op: {
-        id: 'project',
-        title: '作品展示'
+        id: "project",
+        title: "作品展示"
       },
       currentDate: new Date(),
       workOp: [
         {
-          name: 'javaScript',
+          name: "javaScript",
           items: [
             {
-              title: '企业网站',
-              skill: 'javaScript+css',
-              img: 'component-website',
-              link: 'https://zhoubichuan.github.io/Project-JavaScript-CompanyWebsite/'
-            },
-            {
-              title: '后台管理系统',
-              skill: 'jQuery+BootStrapt',
-              img: 'bootStrapt',
+              title: "企业网站",
+              skill: "javaScript+css",
+              img: "component-website",
               link:
-                'https://github.com/zhoubichuan/Project-Bootstrapt-BackManagmentSystem'
+                "https://zhoubichuan.github.io/Project-JavaScript-CompanyWebsite/"
             },
             {
-              title: 'flybird游戏',
-              skill: 'javaScript+canvas+css',
+              title: "后台管理系统",
+              skill: "jQuery+BootStrapt",
+              img: "bootStrapt",
               link:
-                '#'
+                "https://github.com/zhoubichuan/Project-Bootstrapt-BackManagmentSystem"
             },
             {
-              title: '微信小程序',
-              skill: '---',
-              link: 'wwww.123.com'
+              title: "flybird游戏",
+              skill: "canvas+css",
+              img: "fly-bird",
+              link:
+                "https://zhoubichuan.github.io/Project-JavaScript.Canvas-Flybird/"
             },
             {
-              title: '微信公众号',
-              skill: '---',
-              link: 'wwww.123.com'
+              title: "微信跳一跳游戏",
+              skill: "webGL(three.js)+css",
+              img: "tiao-yi-tiao",
+              link: "https://zhoubichuan.github.io/Project-Three.js-TiaoYiTiao/"
+            },
+            {
+              title: "微信小程序",
+              skill: "---",
+              link: "wwww.123.com"
+            },
+            {
+              title: "微信公众号",
+              skill: "---",
+              link: "wwww.123.com"
             }
           ]
         },
         {
-          name: 'Vue',
+          name: "Vue",
           items: [
             {
-              title: 'weChateApp',
-              skill: 'Vue+less',
-              img: 'vue-weChat',
-              link: 'https://zhoubichuan.github.io/Project-Vue-weChat/'
+              title: "weChateApp",
+              skill: "Vue+less",
+              img: "vue-weChat",
+              link: "https://zhoubichuan.github.io/Project-Vue-weChat/"
             },
             {
-              title: 'eleApp',
-              img: 'vue-eleApp',
-              skill: 'Vue+less',
-              link: 'https://zhoubichuan.github.io/Project-Vue-eleSell'
+              title: "eleApp",
+              img: "vue-eleApp",
+              skill: "Vue+less",
+              link: "https://zhoubichuan.github.io/Project-Vue-eleSell"
             },
             {
-              title: 'vue点餐系统',
-              skill: 'Vue+less',
-              link: 'https://zhoubichuan.github.io/Project-Vue-Ordering/'
+              title: "vue点餐系统",
+              skill: "Vue+less",
+              link: "https://zhoubichuan.github.io/Project-Vue-Ordering/"
             }
           ]
         },
         {
-          name: 'React',
+          name: "React",
           items: [
             {
-              title: 'zhufengCourseApp',
-              skill: 'React+TS+less',
-              link: 'https://zhoubichuan.github.io/Project-React.TS-app/'
+              title: "zhufengCourseApp",
+              skill: "React+TS+less",
+              link: "https://zhoubichuan.github.io/Project-React.TS-app/"
             },
             {
-              title: 'weChatApp',
-              skill: 'React+less',
-              link: 'http://zhoubichuan.github.io/Project-React-weChat'
+              title: "weChatApp",
+              skill: "React+less",
+              link: "http://zhoubichuan.github.io/Project-React-weChat"
             }
           ]
         },
         {
-          name: 'Angular',
+          name: "Angular",
           items: [
             {
-              title: '珠峰课堂',
-              link: 'https://zhoubichuan.github.io/Project-React.TS-app/'
+              title: "珠峰课堂",
+              link: "https://zhoubichuan.github.io/Project-React.TS-app/"
             }
           ]
         },
         {
-          name: 'node.js',
+          name: "node.js",
           items: [
             {
-              title: 'CMS系统',
-              skill: 'Egg.js+AntDesignPro+MySQL',
-              link: 'wwww.123.com'
+              title: "CMS系统",
+              skill: "Egg.js+AntDesignPro+MySQL",
+              link: "wwww.123.com"
             }
           ]
         },
         {
-          name: 'express',
+          name: "express",
           items: [
             {
-              title: 'CMS系统',
-              skill: 'Egg.js+AntDesignPro+MySQL',
-              link: 'wwww.123.com'
+              title: "CMS系统",
+              skill: "Egg.js+AntDesignPro+MySQL",
+              link: "wwww.123.com"
             }
           ]
         },
         {
-          name: 'koa',
+          name: "koa",
           items: [
             {
-              title: 'CMS系统',
-              skill: 'Egg.js+AntDesignPro+MySQL',
-              link: 'wwww.123.com'
+              title: "CMS系统",
+              skill: "Egg.js+AntDesignPro+MySQL",
+              link: "wwww.123.com"
             }
           ]
         },
         {
-          name: '全栈项目',
+          name: "全栈项目",
           items: [
             {
-              title: 'CMS系统',
-              skill: 'Egg.js+AntDesignPro+MySQL',
-              link: 'wwww.123.com'
+              title: "CMS系统",
+              skill: "Egg.js+AntDesignPro+MySQL",
+              link: "wwww.123.com"
             },
             {
-              title: '多人聊天室',
-              skill: 'React+Mongodb+websocket',
-              link: 'wwww.123.com'
+              title: "多人聊天室",
+              skill: "React+Mongodb+websocket",
+              link: "wwww.123.com"
             },
             {
-              title: '新闻爬虫',
-              skill: 'cheerio+mysql+nodemailer',
-              link: 'wwww.123.com'
+              title: "新闻爬虫",
+              skill: "cheerio+mysql+nodemailer",
+              link: "wwww.123.com"
             },
             {
-              title: '珠峰课堂',
-              skill: 'React+Redux',
-              link: 'wwww.123.com'
+              title: "珠峰课堂",
+              skill: "React+Redux",
+              link: "wwww.123.com"
             },
             {
-              title: 'flappy bird',
-              skill: 'canvas',
-              link: 'wwww.123.com'
+              title: "flappy bird",
+              skill: "canvas",
+              link: "wwww.123.com"
             },
             {
-              title: '微信跳一跳',
-              skill: 'thress.js',
-              link: 'wwww.123.com'
+              title: "微信跳一跳",
+              skill: "thress.js",
+              link: "wwww.123.com"
             }
           ]
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
