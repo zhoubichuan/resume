ff1a050a46f045dac6e156413a09fbaad9f54276
zhoubichuan <zhoubichuan@icloud.com>
2020-06-20 23:07:30 +0800

    master

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 const path = require('path')
 module.exports = {
-  // 没有书写outputDir属性   默认'dist'  对应dev.assetsSubDirectory.
   outputDir: 'dist',
-
-  // 没有书写baseUrl属性   默认'/'  对应dev.assetsPublicPath.
-  // 从 Vue CLI 3.3 起baseUrl已弃用，请使用publicPath.
   publicPath: './',
   productionSourceMap: false,
   chainWebpack: config => {
@@ -14,7 +10,28 @@ module.exports = {
       .use('html-withimg-loader')
       .loader('html-withimg-loader')
       .end()
-  }
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'), //配置开发服务运行时的文件根目录
+    host: 'localhost', //开发服务器监听的主机地址
+    compress: true, //开发服务器是否启动gzip等压缩
+    port: 8090, //开发服务器监听的端口
+    proxy: { //配置不同的后台API地址
+      // '/api': {
+      //   target: '<url>',
+      //   ws: true,
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     "^/api": "/"
+      //   }
+      // },
+      // '/foo': {
+      //   target: '<other_url>'
+      // }
+    }
+  },
 }
 
 // module.exports = {
