dd6f8e70e3fa6a9ba5a008acefdfcd3155f3e272
周必川 <zhoubichuan@icloud.com>
2023-10-02 16:17:06 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,22 @@
 import Vue from "vue";
 import Router from "vue-router";
-// import Index from "@/components/Index.vue";
+import Index from "@/components/Index.vue";
 
 Vue.use(Router);
 
 export default [
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Index,
-  // },
-  // {
-  //   path: "/demo",
-  //   name: "demo",
-  //   component: (): any => import("@/views/demo/index.vue"),
-  // },
   {
     path: "/",
+    name: "Home",
+    component: Index,
+  },
+  {
+    path: "/demo",
+    name: "demo",
+    component: (): any => import("@/views/demo/index.vue"),
+  },
+  {
+    path: "/echarts",
     name: "echarts",
     component: (): any => import("@/views/echarts/index.vue"),
   },
