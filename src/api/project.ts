0ab68175e3adb7e853786b486254782639abab12
zhoubichuan <zhoubichuan@icloud.com>
2023-10-02 08:28:45 +0000

    Deploying to gh-pages from  @ dd6f8e70e3fa6a9ba5a008acefdfcd3155f3e272 🚀

--- src/api/project.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/request'
-// 获取开源项目
-export function getProject(data: any): any {
-  return request({
-    url: '/mock/project.json',
-    method: 'get',
-    params: data
-  })
-}
--- src/shims-vue.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
-}
-interface Window {
-  __POWERED_BY_QIANKUN__:boolean
-  __INJECTED_PUBLIC_PATH_BY_QIANKUN__:string
-}
