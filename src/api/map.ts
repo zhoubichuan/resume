cea2354d32e4e5e5f971f093583debe893603090
周必川 <zhoubichuan@icloud.com>
2023-10-22 18:21:07 +0800

    update

--- src/api/map.ts
+++ src/api/map.ts
@@ -1,7 +1,7 @@
 import request from "@/util/request";
 export function getMap(index = 1) {
   return request({
-    url: `https://zhoubichuan.com/antdpro-express/api/list/data/${index}?current=1&pageSize=2000`,
+    url: `https://zhoubichuan.com/antdpro-express/api/list/data/8?current=1&pageSize=2000&filterField=map${index}`,
     method: "get",
   });
   // return request({
