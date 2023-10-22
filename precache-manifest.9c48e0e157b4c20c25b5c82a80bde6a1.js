6dacb53fe51ea9fd1e3f3d838c1b8c2eadffeb2c
zhoubichuan <zhoubichuan@icloud.com>
2023-10-22 10:24:31 +0000

    Deploying to gh-pages from  @ cea2354d32e4e5e5f971f093583debe893603090 🚀

---
+++ precache-manifest.9c48e0e157b4c20c25b5c82a80bde6a1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93d040a7da2fe5b92c3",
+    "url": "/resume/dist/css/app.5b8a4c07.css"
+  },
+  {
+    "revision": "17194d1c136d5ff109f9",
+    "url": "/resume/dist/css/chunk-917e67ac.2910e454.css"
+  },
+  {
+    "revision": "c5aeff2017164574c956",
+    "url": "/resume/dist/css/chunk-vendors.4306c048.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/resume/dist/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/resume/dist/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "c93d040a7da2fe5b92c3",
+    "url": "/resume/dist/js/app.c7ef08a4.js"
+  },
+  {
+    "revision": "62e46b2f3fe94aff331d",
+    "url": "/resume/dist/js/chunk-2d0c1aeb.84376fa0.js"
+  },
+  {
+    "revision": "17194d1c136d5ff109f9",
+    "url": "/resume/dist/js/chunk-917e67ac.9a3ff7e3.js"
+  },
+  {
+    "revision": "c5aeff2017164574c956",
+    "url": "/resume/dist/js/chunk-vendors.32fee7b5.js"
+  },
+  {
+    "revision": "6b5d5c24f531558b7efa5271b7fa15c5",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d49cf6b1baa26ed7d400b042944ed7d0",
+    "url": "/resume/manifest.json"
+  },
+  {
+    "revision": "7406bbd54cef0a8f412f2d524f734e59",
+    "url": "/resume/mock/backend.json"
+  },
+  {
+    "revision": "dcbbc60e65fdc1350e22d504efb16134",
+    "url": "/resume/mock/frontend.json"
+  },
+  {
+    "revision": "e7b39ffa80bd9f043cc2a859346eea89",
+    "url": "/resume/mock/operation.json"
+  },
+  {
+    "revision": "30fa1df8ef57c5b66b8f20622760dfb4",
+    "url": "/resume/mock/other.json"
+  },
+  {
+    "revision": "bc6331cc99dab81afe402947950e817a",
+    "url": "/resume/mock/project.json"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.7a6e8edaaa12f3e3d39efdf5ead21c12.js"
+  "/resume/precache-manifest.9c48e0e157b4c20c25b5c82a80bde6a1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "resume"});
